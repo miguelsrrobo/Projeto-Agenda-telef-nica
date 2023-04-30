5377398e5b0d77c1484468b8058a243999f19487
MiguelGSilvestre <54369110+miguelsrrobo@users.noreply.github.com>
2023-04-30 09:11:31 -0300

    Add files via upload

---
+++ memori.cpp
@@ -0,0 +1,12 @@
+#include "i2c.h"
+#include "digital.h"
+#include <stdio.h>
+#include "esp_system.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_spi_flash.h"
+#include "driver/uart.h"
+#include "driver/gpio.h"
+#include "digital.h"
+
